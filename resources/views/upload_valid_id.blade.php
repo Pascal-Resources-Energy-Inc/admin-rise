837ab25965f653cbac4cf2e386364c8c1c48563d
renzcabato1 <cabato.renz.renz@gmail.com>
2026-03-10 23:07:48 +0800

    remove required of id number

--- resources/views/upload_valid_id.blade.php
+++ resources/views/upload_valid_id.blade.php
@@ -10,33 +10,33 @@
       </div>
       <form method='POST' action='{{url('valid-id/'.$customer->id)}}' onsubmit='show()' enctype="multipart/form-data" class="validation-wizard wizard-circle mt-5">
       @csrf
-        <div class="modal-body text-center">
-                     <div class="mb-3">
-                            <label for="valid_id_type" class="form-label">Select ID Type</label>
-                            <select class="form-select" id="valid_id_type" name="valid_id_type" required>
-                            <option selected disabled>Choose valid ID...</option>
-                            <option>Passport</option>
-                            <option>Driver's License</option>
-                            <option>SSS ID</option>
-                            <option>UMID</option>
-                            <option>PhilHealth ID</option>
-                            <option>PRC ID</option>
-                            <option>Postal ID</option>
-                            <option>National ID</option>
-                            <option>Voter's ID</option>
-                            <option>TIN ID</option>
-                            <option>Barangay Clearance</option>
-                            </select>
-                        </div>
-                        <div class="mb-3">
-                            <label for="id_number" class="form-label">ID Number</label>
-                            <input type="text" class="form-control" id="id_number" name="id_number" placeholder="Enter ID number" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="id_file" class="form-label">Upload ID Image</label>
-                            <input type="file" class="form-control" id="id_file" name="id_file" accept="image/*,application/pdf" required>
-                        </div>
-                    </div>
+      <div class="modal-body text-center">
+        <div class="mb-3">
+            <label for="valid_id_type" class="form-label">Select ID Type</label>
+            <select class="form-select" id="valid_id_type" name="valid_id_type" required>
+            <option selected disabled>Choose valid ID...</option>
+            <option>Passport</option>
+            <option>Driver's License</option>
+            <option>SSS ID</option>
+            <option>UMID</option>
+            <option>PhilHealth ID</option>
+            <option>PRC ID</option>
+            <option>Postal ID</option>
+            <option>National ID</option>
+            <option>Voter's ID</option>
+            <option>TIN ID</option>
+            <option>Barangay Clearance</option>
+            </select>
+        </div>
+        <div class="mb-3">
+            <label for="id_number" class="form-label">ID Number</label>
+            <input type="text" class="form-control" id="id_number" name="id_number" placeholder="Enter ID number">
+        </div>
+        <div class="mb-3">
+            <label for="id_file" class="form-label">Upload ID Image</label>
+            <input type="file" class="form-control" id="id_file" name="id_file" accept="image/*,application/pdf" required>
+        </div>
+      </div>
       <div class="modal-footer">
         <button type="button" class="btn bg-danger-subtle text-danger  waves-effect"
           data-bs-dismiss="modal">
